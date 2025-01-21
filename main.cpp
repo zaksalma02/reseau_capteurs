8c1923fe34b607b98f96541615163f93d67fa6fe
TBER Zakaria <Zakaria.Tber@bordeaux-inp.fr>
2025-01-21 15:51:54 +0100

    last_exercice_corige

--- main.cpp
+++ main.cpp
@@ -1,69 +1,51 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
+#define FREQUENCY1   0.5
+#define FREQUENCY2   1.0
 
 Timer t;
 Ticker flipper;
 
 using namespace std::chrono;
 
-// Initialise the digital pin LED1 as an output
+
 #ifdef LED1
     DigitalOut led(LED1);
-#else
-    bool led;
 #endif
+
 
 void flip()
 {
     led = !led;
 }
 
-void freq1(){
-    flipper.attach(&flip, 0.5);
+
+InterruptIn button(BUTTON1);
+
+
+bool toggle_frequency = false;
+
+void change_frequency()
+{
+    toggle_frequency = !toggle_frequency;
+    if (toggle_frequency) {
+        flipper.attach(&flip, FREQUENCY1); 
+    } else {
+        flipper.attach(&flip, FREQUENCY2); 
 }
-
-void freq2(){
-    flipper.attach(&flip, 1.0);
 }
-
-
-//DigitalIn boutton(BUTTON1);
-InterruptIn boutton(BUTTON1);
 
 int main()
 {
-
-//printf("Holaaaaaaa\n");
-
-
-flipper.attach(&flip, 1.0);
-boutton.rise(&freq1);
-boutton.fall(&freq2);
-
-//flipper.attach(&flip, 1.0);
+    // Configuration initiale
+    flipper.attach(&flip, FREQUENCY2); // Démarrer avec la fréquence par défaut
+    button.rise(&change_frequency);   // Attacher une interruption pour le bouton
 
     while (true) {
-    /*
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-	printf("Holaaaaaaa\n");
-	
-	*/
-	printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
-	
-	
-	//printf("l'état du boutton est : %d\n", boutton.read());
+        // Mesurer le temps écoulé si nécessaire
+        printf("The time taken was %llu milliseconds\n", 
+               duration_cast<milliseconds>(t.elapsed_time()).count());
+        
     }
-    
 }
-
-/// rise fall pour les interuptions
