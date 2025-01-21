902712f9b78abcb221ba4320cc04e61ffc8558a1
TBER Zakaria <Zakaria.Tber@bordeaux-inp.fr>
2025-01-21 14:55:23 +0100

    update

--- main.cpp
+++ main.cpp
@@ -10,18 +10,59 @@
 #define BLINKING_RATE     500ms
 
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
+Timer t;
+Ticker flipper;
+
+using namespace std::chrono;
+
+// Initialise the digital pin LED1 as an output
 #ifdef LED1
     DigitalOut led(LED1);
 #else
     bool led;
 #endif
-printf("Holaaaaaaa\n");
+
+void chgmt1(){
+    t.start();
+	led=1;
+}
+void chgmt2(){
+    led=0;
+    t.stop();
+}
+
+void flip()
+{
+    led = !led;
+}
+
+//DigitalIn boutton(BUTTON1);
+InterruptIn boutton(BUTTON1);
+
+int main()
+{
+
+//printf("Holaaaaaaa\n");
+
+
+
+boutton.rise(&chgmt1);
+boutton.fall(&chgmt2);
+flipper.attach(&flip, 1.0);
+
     while (true) {
+    /*
         led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
 	printf("Holaaaaaaa\n");
+	
+	*/
+	printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+	
+	
+	//printf("l'état du boutton est : %d\n", boutton.read());
     }
+    
 }
+
+/// rise fall pour les interuptions
